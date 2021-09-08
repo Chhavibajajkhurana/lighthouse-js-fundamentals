69c01fae1bb95756d8d3eb8a65f215b07587ee50
Chhavibajajkhurana <89600877+Chhavibajajkhurana@users.noreply.github.com>
2021-09-08 01:31:42 -0400

    katas2

---
+++ Katas1.js
@@ -0,0 +1,17 @@
+const sumLargestNumbers = function(array) {
+  // Put your solution here
+  array.sort()
+  return array.last + array[array.length - 2];
+
+};
+
+console.log ( sumLargestNumbers [1,2,3]); 
+
+
+
+
+   
+
+
+
+
---
+++ whichSchool.js
@@ -0,0 +1,13 @@
+func whichSchool()
+
+const age = 13;
+
+if (age < 13) {
+  console.log(" You go to Elementary School");
+
+if (age >= 13 and age <= 18){
+  console.log(" You go to Secondary School");
+} 
+else { 
+  console.log("You go to light house labs");
+}
